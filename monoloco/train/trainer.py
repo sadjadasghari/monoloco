f26d6f1f590359a48f6c480be859935f8f897160
lorenzo <lorenzo.bertoni@yahoo.it>
2019-10-18 10:27:16 +0200

    add xyz loss

--- monoloco/train/trainer.py
+++ monoloco/train/trainer.py
@@ -1,4 +1,3 @@
-
 # pylint: disable=too-many-statements
 """
 Training and evaluation of a neural network which predicts 3D localization and confidence intervals
@@ -16,6 +15,7 @@ import warnings
 import math
 
 import matplotlib
+
 matplotlib.use('module://backend_interagg')
 import matplotlib.pyplot as plt
 import torch
@@ -30,13 +30,13 @@ from ..utils import set_logger
 
 
 class Trainer:
-
     # Constants
     INPUT_SIZE = 34
-    OUTPUT_SIZE = 7
+    OUTPUT_SIZE = 9
     AV_W = 0.68
     AV_L = 0.75
     AV_H = 1.72
+    WLH_STD = 0.1
 
     def __init__(self, joints, epochs=100, bs=256, dropout=0.2, lr=0.002,
                  sched_step=20, sched_gamma=1, hidden_size=256, n_stage=3, r_seed=1, n_samples=100,
@@ -73,9 +73,11 @@ class Trainer:
         name_out = 'monoloco-' + now_time
         self.lambd_ori = 2
         self.lambd_wlh = 1.5
+        self.lambd_xy = 10
 
         # Loss functions
         self.l_loc = LaplacianLoss().cuda()
+        self.l_xy = nn.L1Loss().cuda()
         self.l_ori = nn.L1Loss().cuda()
         self.l_wlh = nn.L1Loss().cuda()
         self.l_eval = nn.L1Loss().cuda()
@@ -146,9 +148,11 @@ class Trainer:
                 for inputs, labels, _, _ in self.dataloaders[phase]:
                     inputs = inputs.to(self.device)
                     labels = labels.to(self.device)
-                    gt_loc = labels[:, 0:1]
-                    gt_ori = labels[:, 1:3]
-                    gt_wlh = labels[:, 4:]
+                    gt_dd = labels[:, 0:1]
+                    gt_xy = labels[:, 1:3]
+                    gt_loc = labels[:, 3:4]
+                    gt_wlh = labels[:, 4:7]
+                    gt_ori = labels[:, 7:9]
 
                     # zero the parameter gradients
                     self.optimizer.zero_grad()
@@ -157,39 +161,49 @@ class Trainer:
                     # track history if only in train
                     with torch.set_grad_enabled(phase == 'train'):
                         outputs = self.model(inputs)
-                        loc = outputs[:, 0:2]
-                        ori = outputs[:, 2:4]
-                        wlh = outputs[:, 4:]
+                        xy = outputs[:, 0:2]
+                        loc = outputs[:, 2:4]
+                        wlh = outputs[:, 4:7]
+                        ori = outputs[:, 7:]
+                        dd = torch.norm(outputs[:, 0:3], p=2, dim=1).view(-1, 1)
 
                         # backward + optimize only if in training phase
-                        loss = self.l_loc(loc, gt_loc) +\
+                        loss = self.l_loc(loc, gt_loc) + \
+                               self.lambd_wlh * self.l_wlh(wlh, gt_wlh) + \
                                self.lambd_ori * self.l_ori(ori, gt_ori) + \
-                               self.lambd_wlh * self.l_wlh(wlh, gt_wlh)
+                               self.lambd_xy * self.l_ori(xy, gt_xy)
 
                         if phase == 'train':
                             loss.backward()
                             self.optimizer.step()
                             running_loss['train']['all'] += loss.item() * inputs.size(0)
                             running_loss['train']['loc'] += self.l_loc(loc, gt_loc).item() * inputs.size(0)
+                            running_loss['train']['xy'] += self.l_xy(xy, gt_xy).item() * inputs.size(0)
                             running_loss['train']['ori'] += self.lambd_ori * self.l_ori(
                                 ori, gt_ori).item() * inputs.size(0)
-                            running_loss['train']['wlh'] += self.l_wlh(wlh, gt_wlh).item() * inputs.size(0)
+                            running_loss['train']['wlh'] += self.l_wlh(wlh, gt_wlh).item() \
+                                                            * inputs.size(0) * self.WLH_STD
                         else:
                             running_loss['val']['all'] += loss.item() * inputs.size(0)
                             running_loss['val']['ori'] += get_angle_loss(ori, gt_ori).item() * inputs.size(0)
-                            running_loss['val']['loc'] += self.l_eval(loc[:, 0:1], gt_loc).item() * inputs.size(0)
-                            running_loss['val']['wlh'] += self.l_eval(wlh, gt_wlh).item() * inputs.size(0)
+                            running_loss['val']['loc'] += self.l_eval(dd, gt_dd).item() * inputs.size(0)
+                            running_loss['val']['xy'] += self.l_xy(xy, gt_xy).item() * inputs.size(0)
+                            running_loss['val']['wlh'] += self.l_eval(wlh, gt_wlh).item() * \
+                                                          inputs.size(0) * self.WLH_STD
 
             for phase in running_loss:
                 for el in running_loss['train']:
                     epoch_losses[phase][el].append(running_loss[phase][el] / self.dataset_sizes[phase])
 
             if epoch % 5 == 1:
-                sys.stdout.write('\r' + 'Epoch: {:.0f} Train Losses: ALL: {:.2f}  Loc: {:.2f} Ori: {:.2f}  Wlh: {:.2f}'
-                                        '         Val Losses: ALL: {:.2f}  Loc: {:.2f}  Ori: {:.2f} WLh: {:.2f}'.format(
-                    epoch, epoch_losses['train']['all'][-1], epoch_losses['train']['loc'][-1],
-                    epoch_losses['train']['ori'][-1], epoch_losses['train']['wlh'][-1],
-                    epoch_losses['val']['all'][-1], epoch_losses['val']['loc'][-1],
+                sys.stdout.write('\r' + 'Epoch: {:.0f} '
+                                        'Train Losses: ALL: {:.2f}  xy: {:.2f} Z: {:.1f} Ori: {:.2f}  Wlh: {:.2f}    '
+                                        '     Val Losses: ALL: {:.2f}  xy: {:.2f}, D: {:.2f}  Ori: {:.2f} WLh: {:.2f}'
+                                 .format(
+                    epoch, epoch_losses['train']['all'][-1], epoch_losses['train']['xy'][-1],
+                    epoch_losses['train']['loc'][-1], epoch_losses['train']['ori'][-1],
+                    epoch_losses['train']['wlh'][-1], epoch_losses['val']['all'][-1],
+                    epoch_losses['val']['xy'][-1], epoch_losses['val']['xy'][-1],
                     epoch_losses['val']['ori'][-1], epoch_losses['val']['wlh'][-1]) + '\t')
 
             # deep copy the model
@@ -200,7 +214,7 @@ class Trainer:
                 best_model_wts = copy.deepcopy(self.model.state_dict())
 
         time_elapsed = time.time() - since
-        print('\n\n' + '-'*120)
+        print('\n\n' + '-' * 120)
         self.logger.info('Training:\nTraining complete in {:.0f}m {:.0f}s'
                          .format(time_elapsed // 60, time_elapsed % 60))
         self.logger.info('Best training Accuracy: {:.3f}'.format(best_training_acc))
@@ -229,7 +243,7 @@ class Trainer:
         size_eval = len(dataset)
         start = 0
         with torch.no_grad():
-            for end in range(batch_size, size_eval+batch_size, batch_size):
+            for end in range(batch_size, size_eval + batch_size, batch_size):
                 end = end if end < size_eval else size_eval
                 inputs, labels, _, _ = dataset[start:end]
                 start = end
@@ -247,11 +261,11 @@ class Trainer:
 
                 dic_err[phase]['all'] = self.compute_stats(outputs, labels, dic_err[phase]['all'], size_eval)
 
-            print('-'*120)
-            self.logger.info("Evaluation, validation set: \nAverage distance {:.2f} m \n"
+            print('-' * 120)
+            self.logger.info("Evaluation, validation set: \nAverage distance xy: {:.1f} m  dd: {:.1f} m \n"
                              "Average orientation: {:.1f} degrees \nAverage dimensions error: {:.0f} cm"
-                             .format(dic_err['val']['all']['loc'], dic_err['val']['all']['ori'],
-                                     dic_err['val']['all']['wlh']*100))
+                             .format(dic_err['val']['all']['xy'], dic_err['val']['all']['dd'],
+                                     dic_err['val']['all']['ori'], dic_err['val']['all']['wlh'] * 100))
 
             # Evaluate performances on different clusters and save statistics
             for clst in self.clusters:
@@ -271,7 +285,7 @@ class Trainer:
         # Save the model and the results
         if self.save and not load:
             torch.save(self.model.state_dict(), self.path_model)
-            print('-'*120)
+            print('-' * 120)
             self.logger.info("\nmodel saved: {} \n".format(self.path_model))
         else:
             self.logger.info("\nmodel not saved\n")
@@ -281,21 +295,28 @@ class Trainer:
     def compute_stats(self, outputs, labels, dic_err, size_eval):
         """Compute mean, bi and max of torch tensors"""
 
-        gt_loc = labels[:, 0:1]
-        gt_ori = labels[:, 1:3]
-        gt_wlh = labels[:, 4:]
-        loc = outputs[:, 0:1]
-        ori = outputs[:, 2:4]
-        wlh = outputs[:, 4:]
+        gt_dd = labels[:, 0:1]
+        gt_xy = labels[:, 1:3]
+        gt_loc = labels[:, 3:4]
+        gt_wlh = labels[:, 4:7]
+        gt_ori = labels[:, 7:9]
+        xy = outputs[:, 0:2]
+        loc = outputs[:, 2:4]
+        wlh = outputs[:, 4:7]
+        ori = outputs[:, 7:]
+        dd = torch.norm(outputs[:, 0:3], p=2, dim=1).view(-1, 1)
 
-        mean_loc = float(self.l_eval(loc, gt_loc).item())
+        mean_loc = float(self.l_eval(loc[:, 0:1], gt_loc).item())
+        mean_dd = float(self.l_eval(dd, gt_dd).item())
+        mean_xy = float(self.l_eval(xy, gt_xy).item())
         mean_ori = float(get_angle_loss(ori, gt_ori).item())
         mean_wlh = float(self.l_eval(wlh, gt_wlh).item())
 
         dic_err['loc'] += mean_loc * (outputs.size(0) / size_eval)
-        # dic_err['bi'] += mean_bi * (outputs.size(0) / size_eval)
+        dic_err['xy'] += mean_xy * (outputs.size(0) / size_eval)
+        dic_err['dd'] += mean_dd * (outputs.size(0) / size_eval)
         dic_err['ori'] += mean_ori * (outputs.size(0) / size_eval)
-        dic_err['wlh'] += mean_wlh * (outputs.size(0) / size_eval)
+        dic_err['wlh'] += mean_wlh * (outputs.size(0) / size_eval) * self.WLH_STD
         dic_err['count'] += (outputs.size(0) / size_eval)
         return dic_err
 
@@ -314,7 +335,6 @@ def debug_plots(inputs, labels):
 
 
 def get_angle_loss(orient, gt_orient):
-
     angle = torch.atan2(orient[:, 0], orient[:, 1]) * 180 / math.pi
     try:
         gt_angle = torch.atan2(gt_orient[:, 0], gt_orient[:, 1]) * 180 / math.pi
@@ -331,4 +351,3 @@ def print_losses(epoch_losses):
             plt.plot(epoch_losses[phase][el][5:], label='{} Loss: {}'.format(phase, el))
             plt.savefig('figures/{}_loss_{}.png'.format(phase, el))
             plt.close()
-
